3a7cfe678802f8f7f7123f9a681636578e89ef62
Ngo Hong Chan <chan@emobi.biz>
2023-03-20 17:52:47 +0700

    Domain layer logic

--- lib/3_application/pages/advice/bloc/cubit/cubit/advicer_cubit.dart
+++ lib/3_application/pages/advice/bloc/cubit/cubit/advicer_cubit.dart
@@ -1,20 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_pattern/1_domain/entities/advice_entity.dart';
+import 'package:flutter_pattern/1_domain/usecases/advice_usecases.dart';
 
 part 'advicer_state.dart';
 
 class AdvicerCubit extends Cubit<AdvicerCubitState> {
   AdvicerCubit() : super(AdviceInitial());
+  final AdviceUseCases adviceUseCases = AdviceUseCases();
+  // could also use other usecases
 
   void adviceRequested() async {
     emit(AdvicerStateLoading());
+    final AdviceEntity advice = await adviceUseCases.getAdvice();
     // execute business logic
     // for example get and advice
     debugPrint('fake get advice triggered');
-    await Future.delayed(const Duration(seconds: 3), () {});
+    // await Future.delayed(const Duration(seconds: 3), () {});
     debugPrint('got advice');
-    emit(AdvicerStateLoaded(advice: 'fake advice test bloc'));
+    emit(AdvicerStateLoaded(advice: advice.advice));
     // emit(AdvicerStateError(message: 'error message'));
   }
 }
